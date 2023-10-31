dc7e1557f8d1dc916553b037b2946326982838d0
FelixMillon <felix.millon@outlook.com>
2023-10-31 15:54:50 +0100

    init

---
+++ MyView/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+const firstname = ref('pierre');
+let isMale = ref(true);
+
+let sampleUser = reactive({
+  username: 'pierre',
+  birthdate: {
+    year: 2022,
+    month: 12,
+    day: 7
+  }
+})
+
+function changeGender(): void {
+  console.log("here");
+  isMale.value = !isMale.value;
+}
+</script>
+
+<template>
+  <main>
+    <div v-if="isMale">
+      bonjour Monsieur
+    </div>
+    <div v-else>
+      bonjour Madame
+    </div>
+    {{firstname}}
+
+    <button @click="$event => changeGender()">Change gender</button>
+  </main>
+</template>
